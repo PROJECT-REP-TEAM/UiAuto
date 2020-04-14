f09dac878710ccbf63a9e92fd13e7eca472b8860
关海敏 <guanhm@legion-tech.net>
2020-04-14 17:22:57 +0800

    选择器更新：解决选择目标元素后获取的元素下标不准确的问题

--- client/public/base_integration/uiauto_uiselector/js/cover_frame.js
+++ client/public/base_integration/uiauto_uiselector/js/cover_frame.js
@@ -52,7 +52,8 @@ try {
             height: bcr['height']
         };
 
-        result.html["xpath"] = generateXPath(el);
+        // console.log(readXPath(el))
+        result.html["xpath"] = readXPath(el)// generateXPath(el);
 
         window["uiauto_selected_result"] = JSON.stringify(result);
 
@@ -70,7 +71,12 @@ try {
             xpath = "//*[@id='" + $(el).attr('id') + "']"
         } else {
             if (el.localName !== 'html') {
-                xpath = generateXPath(el.parentElement) + "/" + el.localName + "[" + $(el).index() + "]"
+                xpath = generateXPath(el.parentElement) + "/" + el.localName
+                var same_tags = $(el).parent().find(el.localName)
+                console.log(same_tags)
+                if (same_tags.length > 0) {
+                    xpath += "[" + $(el).index() + "]"
+                }
             } else {
                 xpath = "//html"
             }
@@ -112,3 +118,29 @@ try {
     console.error(e);
 }
 
+function readXPath(element) {
+    if (element.id !== "") {//判断id属性，如果这个元素有id，则显 示//*[@id="xPath"]  形式内容
+        return "//*[@id='" + element.id + "']";
+    }
+    //这里需要需要主要字符串转译问题，可参考js 动态生成html时字符串和变量转译（注意引号的作用）
+    if (element == document.body) {//递归到body处，结束递归
+        return '/html/' + element.tagName.toLowerCase();
+    }
+    var ix = 0,//在nodelist中的位置，且每次点击初始化
+         siblings = element.parentNode.childNodes;//同级的子元素
+ 
+    for (var i = 0, l = siblings.length; i < l; i++) {
+        var sibling = siblings[i];
+        //如果这个元素是siblings数组中的元素，则执行递归操作
+        if (sibling == element) {
+            if (ix == 0) {
+                return arguments.callee(element.parentNode) + '/' + element.tagName.toLowerCase();
+            } else {
+                return arguments.callee(element.parentNode) + '/' + element.tagName.toLowerCase() + '[' + (ix) + ']';
+            }
+            //如果不符合，判断是否是element元素，并且是否是相同元素，如果是相同的就开始累加
+        } else if (sibling.nodeType == 1 && sibling.tagName == element.tagName) {
+            ix++;
+        }
+    }
+};
