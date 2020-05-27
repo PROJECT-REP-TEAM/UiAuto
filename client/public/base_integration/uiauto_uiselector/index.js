44d2f5c80f80fb17d1a13bc67f42b7ead2a1ab46
guanhm <guanhm@legion-tech.net>
2020-05-27 15:04:43 +0800

    UiAuto选择器2.0.0版本更新： 1、基于C#进行重构。 2、支持直接选择IE浏览器的网页元素，无需通过UiAuto启动浏览器。 3、Chrome浏览器可以添加扩展程序，之后可以直接选择网页元素。

--- client/public/base_integration/uiauto_uiselector/index.js
+++ client/public/base_integration/uiauto_uiselector/index.js
@@ -9,57 +9,50 @@ const listener = new EventEmitter();
 const child_process = require("child_process");
 
 exports.start_process = () => {
-    let options = {
-        mode: 'text',
-        pythonPath: path.join(path.resolve() + "/env/python/win32/python.exe"),
-        // pythonOptions: ["-u"],
-        args: []
-    };
-
-    window['uiselector_py_shell'] = new PythonShell(path.normalize(path.resolve() + "\\public\\base_integration\\uiauto_uiselector\\command.py"), options);
-
-    window['uiselector_py_shell'].stdout.on('data', (data) => {
-        console.log('uiselector stdout data >>>>>>>>>>', data);
-        listener.emit("success", data);
+    console.log(__dirname + "\\UiSelector.exe");
+    if (window.hasOwnProperty("uiselector_shell")) {
+        window['uiselector_shell'] = null;
+    }
+    window['uiselector_shell'] = child_process.execFile(path.resolve(__dirname + "\\UiSelector.exe"), (err, stdout, stderr) => {
+        console.log(err, stdout, stderr);
     });
 
-    window['uiselector_py_shell'].stderr.on('data', (data) => {
+    window['uiselector_shell'].stdout.on("data", (data) => {
+        console.log('uiselector stdout data >>>>>>>>>>', data);
+        const result_file_path = path.join(path.resolve(), "\\public\\base_integration\\uiauto_uiselector\\result.json");
+        if (fs.existsSync(result_file_path)) {
+            const result = fse.readJSONSync(result_file_path);
+            console.log("uiselector selected result", result);
+            listener.emit("success", result);
+            fs.unlinkSync(result_file_path);
+        }
+    });
+
+    window['uiselector_shell'].stderr.on("data", (data) => {
         console.log('uiselector stderr data >>>>>>>>>>', data);
         listener.emit("error", data);
     });
 
-    return window['uiselector_py_shell'];
+    // console.log(child);
+    // child.stdin.write("start\n");
 };
 
 exports.execute = (params) => {
     return new Promise((resolve, reject) => {
         try {
-            const temp_dir = path.normalize(`${path.resolve()}\\.uiauto\\temp\\`);
-            if (!fs.existsSync(temp_dir)) {
-                fs.mkdirSync(temp_dir);
-            }
-            let param_file_name = "uiselector_params.txt";
-            let param_file_path = temp_dir + param_file_name;
-            fs.writeFileSync(param_file_path, JSON.stringify(params));
 
             listener.on("success", (data) => {
-                let result_file_path = param_file_path.replace('.txt', '_result.txt');
-                let result = JSON.parse(fs.readFileSync(result_file_path));
-                if (result) {
-                    resolve(result);
-                } else {
-                    reject('没有选择到元素');
-                }
+                resolve(data);
                 listener.removeAllListeners()
-            });
+            }); 
 
             listener.on("error", (data) => {
                 reject(data);
                 listener.removeAllListeners()
             });
 
-            if (window['uiselector_py_shell']) {
-                window['uiselector_py_shell'].send('execute ' + param_file_path);
+            if (window['uiselector_shell']) {
+                window['uiselector_shell'].stdin.write("start\n");
             } else {
                 reject('选择器没有启动')
             }
@@ -71,45 +64,15 @@ exports.execute = (params) => {
 
 };
 
-exports.remote_browser = (params) => {
-    const temp_dir = path.normalize(`${path.resolve()}\\.uiauto\\temp\\`);
-    if (!fs.existsSync(temp_dir)) {
-        fs.mkdirSync(temp_dir);
-    }
-    let param_file_name = "uiselector_params.txt";
-    let param_file_path = temp_dir + param_file_name;
-    fs.writeFileSync(param_file_path, JSON.stringify(params));
-    if (window['uiselector_py_shell']) {
-        window['uiselector_py_shell'].send('remote_browser ' + param_file_path);
-    } else {
-        console.error('选择器没有启动')
-    }
-}
-
 exports.open_browser = (params) => {
     return new Promise((resolve, reject) => {
         try {
 
             const shell_result = child_process.execSync("REG QUERY HKEY_CURRENT_USER\\SOFTWARE\\Google\\Chrome\\BLBeacon /v version");
             console.log("shell>>>>>>>>>>>>>>>>>", shell_result.toString().split("    "));
-
-            const temp_dir = path.normalize(`${path.resolve()}\\.uiauto\\temp\\`);
-            if (!fs.existsSync(temp_dir)) {
-                fs.mkdirSync(temp_dir);
-            }
-            let param_file_name = "uiselector_params.txt";
-            let param_file_path = temp_dir + param_file_name;
-            params['client_dir'] = path.normalize(path.resolve());
-            fs.writeFileSync(param_file_path, JSON.stringify(params));
         
             listener.on("success", (data) => {
-                let result_file_path = param_file_path.replace('.txt', '_result.txt');
-                let result = JSON.parse(fs.readFileSync(result_file_path));
-                if (result) {
-                    resolve(result);
-                } else {
-                    reject('启动浏览器失败');
-                }
+                resolve(data);
                 listener.removeAllListeners()
             });
         
@@ -118,8 +81,8 @@ exports.open_browser = (params) => {
                 listener.removeAllListeners()
             });
         
-            if (window['uiselector_py_shell']) {
-                window['uiselector_py_shell'].send('open_browser ' + param_file_path);
+            if (window['uiselector_shell']) {
+                window['uiselector_shell'].stdin.write('open_chrome\n');
             } else {
                 reject('选择器没有启动')
             }
@@ -131,11 +94,17 @@ exports.open_browser = (params) => {
 
 exports.restart_process = () => {
     console.log("重启UI选择器");
-    window['uiselector_py_shell'].terminate();
-    exports.start_process();
+    window['uiselector_shell'].stdin.write("exit_app\n");
+    setTimeout(() => {
+        window['uiselector_shell'].kill();
+        exports.start_process();
+    }, 500)
+    
+    
 };
 
 exports.exit_uiselector = () => {
-    window['uiselector_py_shell'].send('exit');
+    window['uiselector_shell'].stdin.write("close_chrome\n");
+    window['uiselector_shell'].kill();
 };
 
