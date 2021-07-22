287e2c75b7ee7a81b7c1728fcc83161eeda1e097
guanhm <guanhm@legion-tech.net>
2021-07-22 18:03:38 +0800

    socket适配nginx转发

--- web/src/express/socket/client/index.js
+++ web/src/express/socket/client/index.js
@@ -50,7 +50,10 @@ export function start_socket_client() {
 
 function start_socket_client_fn() {
   const uiautoConfig = fse.readJsonSync(configPath);
-  socket_client = socket_client || SocketClient(uiautoConfig.serverUrl);
+  const socketUrl = new URL(uiautoConfig.serverUrl);
+  socket_client = socket_client || SocketClient(socketUrl.origin, {
+    path: `${socketUrl.pathname}/socket.io`
+  });
   socket_client.on('connect', function () {
     console.log('connect>>>>>>>>>>>>>>')
     console.log(uiautoConfig.deviceId)
